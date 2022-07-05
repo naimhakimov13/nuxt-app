acc739baad44e2167c90c08f710d752fc20cc0ec
Naim <naimhakimov2001@gmail.com>
2022-07-05 22:37:44 +1000

    'wewer'

--- store/users.js
+++ store/users.js
@@ -9,21 +9,21 @@ export const mutations = {
 }
 
 export const actions = {
-  async fetchUsers({commit}) {
-    try {
-      const users = await this.$axios.$get("http://localhost:4000/users")
-      commit('setUsers', users)
-    } catch (e) {
-      throw e
-    }
-  },
-  async fetchUserById({}, userId) {
-    try {
-      return await this.$axios.$get(`http://localhost:4000/users/${userId}`)
-    } catch (e) {
-      throw e
-    }
-  }
+  // async fetchUsers({commit}) {
+  //   try {
+  //     const users = await this.$axios.$get("http://localhost:4000/users")
+  //     commit('setUsers', users)
+  //   } catch (e) {
+  //     throw e
+  //   }
+  // },
+  // async fetchUserById({}, userId) {
+  //   try {
+  //     return await this.$axios.$get(`http://localhost:4000/users/${userId}`)
+  //   } catch (e) {
+  //     throw e
+  //   }
+  // }
 }
 
 export const getters = {
