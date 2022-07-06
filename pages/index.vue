6ba3768e517e1b91e5fa7a515681a623a188d2b3
naimhakimov13 <108753643+naimhakimov13@users.noreply.github.com>
2022-07-06 15:50:37 +0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,13 @@
   <section>
     <h1>This is home page!</h1>
     <p>loremwerwerwe wer werw rwerwe</p>
+    werwerwerwe
+    werwer
+    werwerwerwerwer
+    werwer
+    wer
+    werwer
+    werwerwer </br>
+    werwerwerwerwe
   </section>
 </template>
