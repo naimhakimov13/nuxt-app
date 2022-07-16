3c3b746bad3b5b208e4db66ed2e4e8d68adad0b4
Naim <naimhakimov2001@gmail.com>
2022-07-16 22:28:04 +1000

    update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,8 @@
   <section>
     <h1>This is home page!</h1>
     <p>loremwerwerwe wer werw rwerwe</p>
-    werwerwerwe
-    werwer
-    werwerwerwerwer
-    werwer
-    wer
-    werwer
-    werwerwerwerwe
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, mollitia.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequuntur debitis dignissimos facere hic
+      in iste itaque natus nisi optio, quam quibusdam sunt, ut, voluptatem voluptatum! Adipisci doloribus magnam
+      soluta.</p>
   </section>
 </template>
